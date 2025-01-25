b070e9e404dcd71b76a1152cf6b1d3c14922e980
Joseph Gargari <joseph.gargari@gmail.com>
2025-01-25 14:54:01 +0100

    chore: divided page into two distinct divs, one for top-half and bottom-half

--- developer_tools/spectrogram_viewer/frontend/spectrogram_viewer_gui/src/pages/MainPage.tsx
+++ developer_tools/spectrogram_viewer/frontend/spectrogram_viewer_gui/src/pages/MainPage.tsx
@@ -16,65 +16,69 @@ import { Tooltip, Button } from '@heroui/react';
 const MainPage = () => {
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex flex-col md:flex-row h-1/2 mx-4 mt-8 space-x-8">
-        <div className="flex-1 flex flex-col relative">
-          <Tabs
-            key="bordered"
-            aria-label="Graph choice"
-            size="md"
-            radius="full"
-            className="ml-4"
-          >
-            <Tab key="spectrogram" title="Spectrogram">
-              <div className="w-full h-full">
-                <img
-                  src={spectrogramImage}
-                  alt="An image of a standard spectrogram"
-                  className="w-full h-96 object-fill shadow-lg rounded-3xl mt-2"
-                />
-              </div>
-            </Tab>
-            <Tab key="DEMON" title="DEMON">
-              <div className="w-full h-full">
-                <img
-                  src={DEMONSpectrogramImage}
-                  alt="An image of a DEMON spectrogram"
-                  className="w-full h-96 object-fill shadow-lg rounded-3xl mt-2"
-                />
-              </div>
-            </Tab>
-          </Tabs>
-          <div className="absolute top-0 right-0 mt-3 mr-4">
-            <Tooltip
-              placement="right"
-              closeDelay={1}
-              delay={1}
-              content="Refreshes current graph, if a new one is available"
+      {/* Top half */}
+      <div className="flex-1">
+        <div className="flex h-full mx-4 mt-8 space-x-8">
+          <div className="flex-1 flex flex-col relative">
+            <Tabs
+              key="bordered"
+              aria-label="Graph choice"
+              size="md"
+              radius="full"
+              className="ml-4"
             >
-              <Button size="sm" radius="full">
-                <img
-                  src={refreshIconImage}
-                  alt="Image of a refresh icon"
-                  className="w-6 h-6"
-                />
-              </Button>
-            </Tooltip>
+              <Tab key="spectrogram" title="Spectrogram">
+                <div className="w-full h-full">
+                  <img
+                    src={spectrogramImage}
+                    alt="An image of a standard spectrogram"
+                    className="w-full h-96 object-fill shadow-lg rounded-3xl mt-2"
+                  />
+                </div>
+              </Tab>
+              <Tab key="DEMON" title="DEMON">
+                <div className="w-full h-full">
+                  <img
+                    src={DEMONSpectrogramImage}
+                    alt="An image of a DEMON spectrogram"
+                    className="w-full h-96 object-fill shadow-lg rounded-3xl mt-2"
+                  />
+                </div>
+              </Tab>
+            </Tabs>
+            <div className="absolute top-0 right-0 mt-3 mr-4">
+              <Tooltip
+                placement="right"
+                closeDelay={1}
+                delay={1}
+                content="Refreshes current graph, if a new one is available"
+              >
+                <Button size="sm" radius="full">
+                  <img
+                    src={refreshIconImage}
+                    alt="Image of a refresh icon"
+                    className="w-6 h-6"
+                  />
+                </Button>
+              </Tooltip>
+            </div>
           </div>
-        </div>
 
-        <div className="flex-1 relative">
-          <div className="w-full h-full">
-            <img
-              src={mapImage}
-              alt="Map with AIS data"
-              className="w-full h-96 object-fill shadow-lg rounded-3xl mt-14"
-            />
+          <div className="flex-1 relative">
+            <div className="w-full h-full">
+              <img
+                src={mapImage}
+                alt="Map with AIS data"
+                className="w-full h-96 object-fill shadow-lg rounded-3xl mt-14"
+              />
+            </div>
           </div>
         </div>
       </div>
 
-      <div className=" flex flex-1 justify-end items-end  mt-2  ml-8 px-4">
-        <div className="relative bottom-44 w-full md:w-1/2 p-4  mb-96 max-h-screen overflow-auto">
+      {/* Bottom half */}
+      <div className="flex-1 flex justify-end items-center px-4">
+        <div className="relative w-1/2 p-4 bottom-96 mt-8">
           <Table aria-label="10 nearest AIS signals">
             <TableHeader>
               <TableColumn>MMSI</TableColumn>
