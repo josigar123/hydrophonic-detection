28199e96704c4fdf248a5868e53531b379fee6b5
Joseph Gargari <joseph.gargari@gmail.com>
2025-02-20 16:21:32 +0100

    fix unrecognized format reading raw PCM data into Web Audio API

--- code_on_raspberry_pi/websocket_server/websocket_server.py
+++ code_on_raspberry_pi/websocket_server/websocket_server.py
@@ -1,8 +1,6 @@
 import asyncio
 import websockets
 import pyaudio
-import wave
-import io
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
@@ -24,7 +22,6 @@ p.terminate()
 DEVICE_INDEX = int(input("Select an index from the above list:"))
 print("#############SETUP END#############")
 
-
 async def stream_audio(websocket):
     p = pyaudio.PyAudio()
     
@@ -35,27 +32,12 @@ async def stream_audio(websocket):
                     input_device_index=DEVICE_INDEX,
                     frames_per_buffer=CHUNK)
 
-    print("Streaming audio...")
+    print("Streaming raw PCM audio...")
 
     try:
-        wav_buffer = io.BytesIO()
-        
-        wav_writer = wave.open(wav_buffer, 'wb')
-        wav_writer.setnchannels(CHANNELS)
-        wav_writer.setsampwidth(p.get_sample_size(FORMAT))
-        wav_writer.setframerate(RATE)
-
         while True:
             data = stream.read(CHUNK, exception_on_overflow=False)
-
-            wav_writer.writeframes(data)
-
-            wav_data = wav_buffer.getvalue()
-            
-            await websocket.send(wav_data)
-            
-            wav_buffer.seek(0)
-            wav_buffer.truncate()
+            await websocket.send(data)
 
     except websockets.exceptions.ConnectionClosed:
         print("Client disconnected")
@@ -65,7 +47,6 @@ async def stream_audio(websocket):
         stream.close()
         p.terminate()
         print("Stopped streaming")
-
 
 async def main():
     async with websockets.serve(stream_audio, IP, PORT):
