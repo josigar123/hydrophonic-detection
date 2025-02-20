ff99bda41be32312972baec47d7d7da841380909
Joseph Gargari <joseph.gargari@gmail.com>
2025-02-20 15:43:11 +0100

    Add user gesture so AudioContext is happy

--- app/frontend/spectrogram_viewer_gui/src/Components/AudioRecorder.tsx
+++ app/frontend/spectrogram_viewer_gui/src/Components/AudioRecorder.tsx
@@ -1,31 +1,24 @@
-import { useState, useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { SmoothieChart, TimeSeries } from 'smoothie';
+import { useAudioStream } from '../Hooks/useAudioStream';
+import { Button } from '@heroui/button';
 
 // Nice bg colour: #232323
 
-const mimeType = 'audio/webm';
+const websocketUrl = 'ws://10.0.0.13:8765';
 
 const AudioRecorder = () => {
-  const [permission, setPermission] = useState(false);
-  const [stream, setStream] = useState<MediaStream | null>(null);
-  const mediaRecorder = useRef<MediaRecorder | null>(null);
-  const [recordingStatus, setRecordingStatus] = useState('inactive');
-  const audioChunks = useRef<Blob[]>([]);
-  const [audio, setAudio] = useState<string | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-
   const smoothieChartRef = useRef<SmoothieChart | null>(null);
   const timeSeriesRef = useRef<TimeSeries | null>(null);
   const audioCtxRef = useRef<AudioContext | null>(null);
   const analyserNodeRef = useRef<AnalyserNode | null>(null);
   const animationFrameRef = useRef<number | null>(null);
+  const { audioData, isConnected } = useAudioStream(websocketUrl);
+  const [listening, setListening] = useState(false);
 
   useEffect(() => {
-    getMicrophonePermission();
-  }, []);
-
-  useEffect(() => {
-    if (!canvasRef.current || !stream) return;
+    if (!canvasRef.current || !isConnected) return;
     if (audioCtxRef.current) {
       audioCtxRef.current.close();
     }
@@ -33,53 +26,64 @@ const AudioRecorder = () => {
       cancelAnimationFrame(animationFrameRef.current);
     }
 
-    audioCtxRef.current = new AudioContext();
-    analyserNodeRef.current = audioCtxRef.current.createAnalyser();
-    analyserNodeRef.current.fftSize = 256;
+    if (listening) {
+      audioCtxRef.current = new AudioContext();
+      analyserNodeRef.current = audioCtxRef.current.createAnalyser();
+      analyserNodeRef.current.fftSize = 256;
 
-    const source = audioCtxRef.current.createMediaStreamSource(stream);
-    source.connect(analyserNodeRef.current);
+      if (audioData == null) return;
 
-    if (!smoothieChartRef.current) {
-      smoothieChartRef.current = new SmoothieChart({
-        grid: {
-          strokeStyle: 'rgb(125, 0, 0)',
-          fillStyle: 'rgb(60, 0, 0)',
-          lineWidth: 1,
-          millisPerLine: 250,
-          verticalSections: 6,
-        },
-        labels: { fillStyle: 'rgb(60, 0, 0)' },
+      audioCtxRef.current.decodeAudioData(audioData.buffer, (buffer) => {
+        const source = audioCtxRef.current!.createBufferSource();
+        source.buffer = buffer;
+
+        if (analyserNodeRef.current) {
+          source.connect(analyserNodeRef.current);
+        }
+        source.start();
       });
-      smoothieChartRef.current.streamTo(canvasRef.current);
+
+      const bufferLength = analyserNodeRef.current.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+
+      if (!smoothieChartRef.current) {
+        smoothieChartRef.current = new SmoothieChart({
+          grid: {
+            strokeStyle: 'rgb(125, 0, 0)',
+            fillStyle: 'rgb(60, 0, 0)',
+            lineWidth: 1,
+            millisPerLine: 250,
+            verticalSections: 6,
+          },
+          labels: { fillStyle: 'rgb(60, 0, 0)' },
+        });
+        smoothieChartRef.current.streamTo(canvasRef.current);
+      }
+
+      if (!timeSeriesRef.current) {
+        timeSeriesRef.current = new TimeSeries();
+        smoothieChartRef.current.addTimeSeries(timeSeriesRef.current, {
+          strokeStyle: 'rgb(0, 255, 0)',
+          fillStyle: 'rgba(0, 255, 0, 0.4)',
+          lineWidth: 3,
+        });
+      }
+
+      const updateChart = () => {
+        if (!analyserNodeRef.current || !timeSeriesRef.current) return;
+
+        analyserNodeRef.current.getByteTimeDomainData(dataArray);
+
+        const average =
+          dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
+
+        timeSeriesRef.current.append(new Date().getTime(), average);
+
+        animationFrameRef.current = requestAnimationFrame(updateChart);
+      };
+
+      updateChart();
     }
-
-    if (!timeSeriesRef.current) {
-      timeSeriesRef.current = new TimeSeries();
-      smoothieChartRef.current.addTimeSeries(timeSeriesRef.current, {
-        strokeStyle: 'rgb(0, 255, 0)',
-        fillStyle: 'rgba(0, 255, 0, 0.4)',
-        lineWidth: 3,
-      });
-    }
-
-    const bufferLength = analyserNodeRef.current.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    const updateChart = () => {
-      if (!analyserNodeRef.current || !timeSeriesRef.current) return;
-
-      analyserNodeRef.current.getByteTimeDomainData(dataArray);
-
-      const average =
-        dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
-
-      timeSeriesRef.current.append(new Date().getTime(), average);
-
-      animationFrameRef.current = requestAnimationFrame(updateChart);
-    };
-
-    updateChart();
 
     return () => {
       if (audioCtxRef.current) {
@@ -90,64 +94,14 @@ const AudioRecorder = () => {
         cancelAnimationFrame(animationFrameRef.current);
       }
     };
-  }, [stream]);
+  }, [audioData, isConnected, listening]);
 
-  const getMicrophonePermission = async () => {
-    if ('MediaRecorder' in window) {
-      try {
-        const streamData = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-          video: false,
-        });
-        setPermission(true);
-        setStream(streamData);
-      } catch (error) {
-        alert(error);
-      }
-    } else {
-      alert('Audio will not work since you did not accept');
-    }
+  const handleStartListening = () => {
+    setListening(true);
   };
 
-  const startRecording = async () => {
-    try {
-      setRecordingStatus('recording');
-
-      if (!stream) {
-        throw new Error('No audio permission');
-      }
-      mediaRecorder.current = new MediaRecorder(stream, { mimeType });
-
-      audioChunks.current = [];
-
-      mediaRecorder.current.ondataavailable = (event) => {
-        if (event.data.size > 0 && !(typeof event.data === 'undefined')) {
-          audioChunks.current?.push(event.data);
-        }
-      };
-
-      mediaRecorder.current.start();
-    } catch (error) {
-      console.error('Error during recording:', error);
-    }
-  };
-
-  const stopRecording = async () => {
-    try {
-      setRecordingStatus('inactive');
-
-      if (!mediaRecorder.current) {
-        throw new Error('No media recorder available');
-      }
-      mediaRecorder.current.stop();
-      mediaRecorder.current.onstop = () => {
-        const audioBlob = new Blob(audioChunks.current, { type: mimeType });
-        const audioUrl = URL.createObjectURL(audioBlob);
-        setAudio(audioUrl);
-      };
-    } catch (error) {
-      console.error('Error during stop recording:', error);
-    }
+  const handleStopListening = () => {
+    setListening(false);
   };
 
   return (
@@ -158,6 +112,8 @@ const AudioRecorder = () => {
         height="300"
         className="bg-[#232323] rounded-lg mb-4"
       ></canvas>
+      <Button onPress={handleStartListening}>Start Listening</Button>
+      <Button onPress={handleStopListening}>Stop Listening</Button>
     </div>
   );
 };
