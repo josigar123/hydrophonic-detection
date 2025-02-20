ac3064a22c04952bfccd78a1dc70367d1af1e721
Joseph Gargari <joseph.gargari@gmail.com>
2025-02-20 15:46:26 +0100

    Format PCM to WAV before sending chunk through WS

--- code_on_raspberry_pi/websocket_server/websocket_server.py
+++ code_on_raspberry_pi/websocket_server/websocket_server.py
@@ -1,16 +1,17 @@
 import asyncio
 import websockets
 import pyaudio
+import wave
+import io
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 CHUNK = 1024
-
 PORT = 8765
 
 print("#############SERVER SETUP#############")
-IP=input("Enter private ip:")
+IP = input("Enter private ip:")
 
 print("Audio devices found:")
 p = pyaudio.PyAudio()
@@ -22,6 +23,7 @@ for i in range(p.get_device_count()):
 p.terminate()
 DEVICE_INDEX = int(input("Select an index from the above list:"))
 print("#############SETUP END#############")
+
 
 async def stream_audio(websocket):
     p = pyaudio.PyAudio()
@@ -36,9 +38,24 @@ async def stream_audio(websocket):
     print("Streaming audio...")
 
     try:
+        wav_buffer = io.BytesIO()
+        
+        wav_writer = wave.open(wav_buffer, 'wb')
+        wav_writer.setnchannels(CHANNELS)
+        wav_writer.setsampwidth(p.get_sample_size(FORMAT))
+        wav_writer.setframerate(RATE)
+
         while True:
             data = stream.read(CHUNK, exception_on_overflow=False)
-            await websocket.send(data)
+
+            wav_writer.writeframes(data)
+
+            wav_data = wav_buffer.getvalue()
+            
+            await websocket.send(wav_data)
+            
+            wav_buffer.seek(0)
+            wav_buffer.truncate()
 
     except websockets.exceptions.ConnectionClosed:
         print("Client disconnected")
@@ -48,6 +65,7 @@ async def stream_audio(websocket):
         stream.close()
         p.terminate()
         print("Stopped streaming")
+
 
 async def main():
     async with websockets.serve(stream_audio, IP, PORT):
