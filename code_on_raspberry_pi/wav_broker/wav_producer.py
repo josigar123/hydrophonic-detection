96e53ed3f9bf18eb3dfe8b10c1792e89c3bde414
Joseph Gargari <joseph.gargari@gmail.com>
2025-02-27 15:34:27 +0100

    Push code to producer

--- code_on_raspberry_pi/wav_broker/wav_producer.py
+++ code_on_raspberry_pi/wav_broker/wav_producer.py
@@ -1,7 +1,19 @@
+import time
 from kafka import KafkaProducer
 
 producer = KafkaProducer(
-    bootstrap_servers=["10.0.0.13:9092"]
+    bootstrap_servers=["10.0.0.13:9092"],
+    value_serializer=lambda v: str(v).encode('utf-8')
 )
 
-producer.send("quickstart-events", value="Hello, World!".encode("utf-8"))
+def produce_stream():
+    counter = 0
+    while True:
+        message = f"Message {counter}"
+        producer.send("quickstart-events", value=message)
+        print(f"Sent: {message}")
+        counter += 1
+        
+        time.sleep(1)
+
+produce_stream()
