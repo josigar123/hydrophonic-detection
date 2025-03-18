badaa15da9dc7d06e05159b2884f9ea01b5315e6
Joseph Gargari <joseph.gargari@gmail.com>
2025-03-18 16:25:21 +0100

    wip: proper spectrogram and demon view
    Working on retrieving params, need to send them, ensure all fields are filled before making a connection, still very rough atm, but wont take too long

--- app/frontend/spectrogram_viewer_gui/src/Pages/MainPage.tsx
+++ app/frontend/spectrogram_viewer_gui/src/Pages/MainPage.tsx
@@ -4,13 +4,15 @@ import WaveformSelection from '../Components/WaveformSelection';
 import DataTransferTest from '../Components/DataTransferTest';
 import ScrollingSpectrogram from '../Components/ScrollingSpectrogram';
 import ImprovedWaveform from '../Components/ImprovedWaveform';
+import SpectrogramSelection from '../Components/SpectrogramSelection';
 
 const MainPage = () => {
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-2 lg:gap-4 w-full h-screen p-2 lg:p-4">
       <div className="min-h-0 min-w-0 overflow-auto p-4 rounded">
         {/* <DataTransferTest></DataTransferTest> */}
-        <ScrollingSpectrogram />
+        {/* <ScrollingSpectrogram /> */}
+        <SpectrogramSelection />
       </div>
       <div className="min-h-0 min-w-0 overflow-auto p-4 rounded">
         {/*<MapComponent />*/}
