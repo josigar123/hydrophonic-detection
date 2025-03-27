1a7a59058c6a35049ff627a7bc75f07e78c69001
Victor Dehli <dehvic@gmail.com>
2025-03-27 13:25:35 +0100

    fix(metadata-storage): Changed logic for handling relevant ais-data
    Fix faulty timestamp comparison
    Fix wrong timestamp for ais-entries

--- app/frontend/spectrogram_viewer_gui/src/Components/MapComponent.tsx
+++ app/frontend/spectrogram_viewer_gui/src/Components/MapComponent.tsx
@@ -3,8 +3,6 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import ShipMarker, { Ship } from './ShipMarker';
 import { useShips } from '../Hooks/useShips';
 
-const MAX_RANGE = 200; // km
-
 const MapComponent = () => {
   const [center] = useState<[number, number]>([59.431633, 10.478039]);
   const { ships } = useShips();
