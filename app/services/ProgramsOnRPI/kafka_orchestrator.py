1c9d0a5741aa397dd1ad4c0409368dbda62408c8
Joseph Gargari <joseph.gargari@gmail.com>
2025-04-13 15:43:22 +0200

    fix prog on RPI

--- app/services/ProgramsOnRPI/kafka_orchestrator.py
+++ app/services/ProgramsOnRPI/kafka_orchestrator.py
@@ -1,6 +1,6 @@
-from config_producer import produce_audio_config
-from audio_producer import produce_audio, get_device_index
-from topic_creator import create_topic
+from KafkaProducers.config_producer import produce_audio_config
+from KafkaProducers.audio_producer import produce_audio, get_device_index
+from KafkaUtils.topic_creator import create_topic
 import json
 
 '''
@@ -10,8 +10,8 @@ then produce audio continously
 
 '''
 
-BROKER_INFO_FILE = "broker_info.json"
-RECORDING_PARAMETERS_FILE = "recording_parameters.json"
+BROKER_INFO_RELATIVE_PATH = "configs/broker_info.json"
+RECORDING_PARAMETERS_RELATIVE_PATH = "configs/recording_parameters.json"
 
 RECORDING_PARAMETERS_TOPIC = "recording-parameters"
 AUDIO_STREAM_TOPIC = "audio-stream"
@@ -23,10 +23,10 @@ USER_POSITION_TOPIC = "user-position"
 RECORDING_STATUS_TOPIC = "recording-status"
 
 if __name__ == "__main__":
-    with open(BROKER_INFO_FILE, "r") as file:
+    with open(BROKER_INFO_RELATIVE_PATH, "r") as file:
         broker_info = json.load(file)
 
-    with open(RECORDING_PARAMETERS_FILE, "r") as file:
+    with open(RECORDING_PARAMETERS_RELATIVE_PATH, "r") as file:
         recording_parameters = json.load(file)
 
     recording_parameters_config = {
@@ -44,10 +44,10 @@ if __name__ == "__main__":
     create_topic(broker_info, RECORDING_STATUS_TOPIC)
 
     # Produce the config to the topic before all else
-    produce_audio_config(broker_info, "recording-parameters", recording_parameters, key="config")
+    produce_audio_config(broker_info, RECORDING_PARAMETERS_TOPIC, recording_parameters, key="config")
 
     # Fetch index of the device to listen to
     device_index = get_device_index()
 
     # Start producing audio to the topic from the desired device index
-    produce_audio(broker_info, "audio-stream", recording_parameters, device_index)
+    produce_audio(broker_info, AUDIO_STREAM_TOPIC, recording_parameters, device_index)
